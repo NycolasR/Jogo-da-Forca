f50ca7c013f8b9690def955b854979bbd9bcbac7
NycolasR <nycolasramon3@gmail.com>
2020-06-28 21:46:04 -0300

    Correção da palavra "comentário" feita pelo Eclipse.

--- Jogo da Forca/src/main/Main.java
+++ Jogo da Forca/src/main/Main.java
@@ -5,6 +5,7 @@ import telas.TelaLogin;
 public class Main {
 	public static void main(String[] args) {
 		new TelaLogin();
-		// comet√°rio teste feito no GitHub
+		// coment·rio teste feito no GitHub
+		// palavra "coment·rio" corrigida no Eclipse.
 	}
 }
